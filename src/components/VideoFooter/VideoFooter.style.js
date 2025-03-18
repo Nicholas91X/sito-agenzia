c43c230c3974c0d5ddf5be4d89053ae6f7510f89
Nicholas91X <romitinicholas91@hotmail.it>
2025-03-18 21:01:05 +0100

    Sistemato VideoFooter

--- src/components/VideoFooter/VideoFooter.style.js
+++ src/components/VideoFooter/VideoFooter.style.js
@@ -4,7 +4,14 @@ import { styled } from '@mui/material/styles';
 export const BoxVideoFooter = styled('div')(({ theme }) => ({
     position: 'relative',
     width: '100%',
-    minHeight: '20vh',
+    // Altezza responsive: più piccola sui dispositivi mobili, più alta su desktop
+    height: '20vh',
+    [theme.breakpoints.up('sm')]: {
+        height: '20vh',
+    },
+    [theme.breakpoints.up('md')]: {
+        height: '20vh',
+    },
     overflow: 'hidden',
 }));
 
@@ -29,9 +36,10 @@ export const FooterOverlay = styled(Box)(({ theme }) => ({
 
 export const FooterContent = styled(Box)(({ theme }) => ({
     position: 'absolute',
-    bottom: -10,
+    bottom: 0,
     left: 0,
     width: '100%',
-    height: '100%',
+    padding: theme.spacing(2),
     zIndex: 2,
+    textAlign: 'center',
 }));
