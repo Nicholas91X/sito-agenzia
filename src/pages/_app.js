13366ab81bbcae4c5f29f2b7edc36cc11df341f3
Nicholas91X <romitinicholas91@hotmail.it>
2025-03-20 19:04:36 +0100

    Rimosso ThemeProvider duplicato

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,10 +13,8 @@ export default function MyApp({ Component, pageProps }) {
                 <Head>
                     <meta name="viewport" content="initial-scale=1, width=device-width" />
                 </Head>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Component {...pageProps} />
-                </ThemeProvider>
+                <CssBaseline />
+                <Component {...pageProps} />
             </ThemeProvider>
         </>
     );
